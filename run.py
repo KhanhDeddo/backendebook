375c51c8e579cd5ca29d9e6bbc3fd3d182cb60e3
Khanh Deddo <khanhdeddo@gmail.com>
2024-12-26 12:57:44 +0700

    bnm

--- run.py
+++ run.py
@@ -20,18 +20,20 @@ def get_db_connection():
 
 @app.route('/')
 def index():
-    # Truy vấn cơ sở dữ liệu
-    conn = get_db_connection()
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute('SELECT * FROM Users')  # Thay 'your_table' bằng tên bảng của bạn
-    rows = cursor.fetchall()
-    cursor.close()
-    conn.close()
+    # # Truy vấn cơ sở dữ liệu
+    # conn = get_db_connection()
+    # cursor = conn.cursor(dictionary=True)
+    # cursor.execute('SELECT * FROM Users')  # Thay 'your_table' bằng tên bảng của bạn
+    # rows = cursor.fetchall()
+    # cursor.close()
+    # conn.close()
     
-    # Trả về dữ liệu dưới dạng JSON
-    return jsonify(rows)
+    # # Trả về dữ liệu dưới dạng JSON
+    # return jsonify(rows)
+    return "haha"
 
 from waitress import serve
 
 if __name__ == "__main__":
+    # app.run(debug=True)
     serve(app, host='0.0.0.0', port=5000)  # Sử dụng Waitress để phục vụ ứng dụng
