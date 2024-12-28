d372f1eac43689d3d316e71dad47ce14303b27f1
Khanh Deddo <khanhdeddo@gmail.com>
2024-12-28 08:56:35 +0700

    updatebykhanhdeddo

--- app/routes.py
+++ app/routes.py
@@ -430,8 +430,25 @@ def dashboard():
     ), 201
 
 # ----------------------------------------------------------------------------------
-# Route Admin
+#//Region Route Admin
 
+# Route cho dashboard
+@api_bp.route('/dashboard', methods=['GET'])
+def dashboard():
+    orders = Order.query.count()
+    books  =  Book.query.all()
+    users = User.query.count()
+    item = 0
+    for book in books:
+        item += book.stock_quantity
+    return jsonify(
+        {
+            "order": orders,
+            "book" : len(books),
+            "user" : users,
+            "item": item
+        }
+    ), 201
 # Route để lấy danh sách 5 đơn hàng gần đây
 LIMIT_RECENT = 5
 @api_bp.route('/order-recent', methods=['GET'])
@@ -473,3 +490,4 @@ def get_adminorder():
         else:
             order['user_name'] = user.user_name
     return jsonify(orders_list), 200
+
